0dafe89ef2ac5ed953e68519d8f5ce3f8e48d7ac
Fábio Mendes <fabiobends@gmail.com>
2021-06-08 00:04:01 -0300

    Add fake api for dealing with image

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express, { NextFunction, Request, Response } from "express";
+import multer from "multer";
 import "reflect-metadata";
+import multerConfig from "./config/multer";
+import { router } from "./routes";
 
 const app = express();
+const upload = multer(multerConfig);
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
-app.get("/", (request: Request, response: Response, _next: NextFunction) => {
-  response.json({ message: "hello backend" });
-});
+app.post(
+  "/articles",
+  upload.single("image"),
+  (request: Request, response: Response, _next: NextFunction) => {
+    console.log(request.file);
+    response.json({ message: "image uploaded" });
+  }
+);
 
 export { app };
