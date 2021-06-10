025476d15b9de12c1936383eefcdb20d9a5140c2
Fábio Mendes <fabiobends@gmail.com>
2021-06-09 22:16:32 -0300

    Add fake stats and dashboard plot

--- src/controllers/FakeStatsController.ts
+++ src/controllers/FakeStatsController.ts
@@ -1,1 +1,122 @@
+import { Request, Response } from "express";
+import path from "path";
+
+class FakeStatsController {
+  dashboardPlot(request: Request, response: Response) {
+    const auxPath = path.resolve(__dirname, "..", "..");
+    const plot = auxPath + `/uploads/graphics.png`;
+
+    response.sendFile(plot);
+  }
+
+  dashboard(request: Request, response: Response) {
+    const data = [
+      {
+        title: "Published articles",
+        amount: 15,
+        color: "#21C8D4",
+        progress: 25,
+      },
+
+      {
+        title: "Sales of articles",
+        amount: 300,
+        color: "#FEC72E",
+        progress: 75,
+      },
+
+      { title: "Read articles", amount: 10, color: "#FD8453", progress: 50 },
+
+      {
+        title: "Purchased articles",
+        amount: 8,
+        color: "#1E3A6F",
+        progress: 25,
+      },
+
+      {
+        title: "Citations received",
+        amount: 30,
+        color: "#F697B5",
+        progress: 50,
+      },
+    ];
+
+    return response.json(data);
+  }
+
+  purchasedArticles(request: Request, response: Response) {
+    const data = [
+      {
+        title: "Strategy",
+        amount: 2,
+        color: "#21C8D4",
+        progress: 25,
+      },
+
+      {
+        title: "Design",
+        amount: 10,
+        color: "#FEC72E",
+        progress: 75,
+      },
+
+      { title: "Development", amount: 8, color: "#FD8453", progress: 50 },
+
+      {
+        title: "Social",
+        amount: 6,
+        color: "#1E3A6F",
+        progress: 25,
+      },
+
+      {
+        title: "Business",
+        amount: 30,
+        color: "#F697B5",
+        progress: 50,
+      },
+    ];
+
+    return response.json(data);
+  }
+
+  articlesYouWrote(request: Request, response: Response) {
+    const data = [
+      {
+        title: "Strategy",
+        amount: 2,
+        color: "#21C8D4",
+        progress: 25,
+      },
+
+      {
+        title: "Design",
+        amount: 10,
+        color: "#FEC72E",
+        progress: 75,
+      },
+
+      { title: "Development", amount: 8, color: "#FD8453", progress: 50 },
+
+      {
+        title: "Social",
+        amount: 6,
+        color: "#1E3A6F",
+        progress: 25,
+      },
+
+      {
+        title: "Business",
+        amount: 30,
+        color: "#F697B5",
+        progress: 50,
+      },
+    ];
+
+    return response.json(data);
+  }
+}
+
+export { FakeStatsController };
 // TODO: create fake stats for user
--- src/routes.ts
+++ src/routes.ts
@@ -2,27 +2,28 @@ import { NextFunction, Request, Response, Router } from "express";
 import multer from "multer";
 import multerConfig from "./config/multer";
 import { ArticlesController } from "./controllers/ArticlesController";
+import { FakeStatsController } from "./controllers/FakeStatsController";
 
 const upload = multer(multerConfig);
 const router = Router();
 const articlesController = new ArticlesController();
+const fakeStatsController = new FakeStatsController();
 
 router.get("/", (request: Request, response: Response, _next: NextFunction) => {
   response.json({ message: "hello from backend" });
 });
 
-router.get("/dashboard", (request: Request, response: Response) => {
-  // TODO: fake stats
-});
+// fake stats - dashboard
+router.get("/dashboard", fakeStatsController.dashboard);
 
-router.get("/purchased-articles", (request: Request, response: Response) => {
-  // TODO: fake stats
-});
+// fake stats - dashboard plot 
+router.get("/dashboard-plot", fakeStatsController.dashboardPlot);
 
-router.get("/articles-you-wrote", (request: Request, response: Response) => {
-  // TODO: fake stats
-  // I may add an author id...
-});
+// fake stats - purchased articles
+router.get("/purchased-articles", fakeStatsController.purchasedArticles);
+
+// fake stats - articles you wrote
+router.get("/articles-you-wrote", fakeStatsController.articlesYouWrote);
 
 // create new article
 router.post("/article", upload.single("image"), articlesController.create);
